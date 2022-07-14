9a9149fb4995e31a006f6f924d7d127cdc0169ed
kenjis <kenji.uui@gmail.com>
2022-07-14 17:09:51 +0900

    feat: ci4-install.php command

---
+++ README.md
@@ -0,0 +1,13 @@
+# CodeIgniter4 Composer Installer
+
+You can install previous versions of [CodeIgniter 4 app starter](https://github.com/codeigniter4/appstarter).
+
+## Usage
+
+```console
+$ php ci4-install.php <dir> <version>
+```
+
+```console
+$ php ci4-install.php ci4app 4.1.9
+```
---
+++ ci4-install.php
@@ -0,0 +1,33 @@
+#!/usr/bin/env php
+<?php
+
+if ($argc < 3) {
+    file_put_contents(
+        'php://stderr',
+        'Usage: php ci4-install.php <directory> <version>' . PHP_EOL
+        . '   Eg: php ci4-install.php ci4app 4.1.9' . PHP_EOL
+    );
+
+    exit(1);
+}
+
+$dir = $argv[1];
+$version = $argv[2];
+
+$command = 'composer create-project codeigniter4/appstarter:v' . $version . ' ' . $dir;
+echo $command . PHP_EOL;
+passthru($command);
+
+$command = 'cd ' . $dir . '; composer require -W codeigniter4/framework:v' . $version;
+echo $command . PHP_EOL;
+passthru($command);
+
+echo 'update "codeigniter4/framework" version in composer.json'. PHP_EOL;
+$composerJsonPath = "$dir/composer.json";
+$composerJson = file_get_contents($composerJsonPath);
+$newComposerJson = preg_replace(
+    '!"codeigniter4/framework": ".*?"!',
+    '"codeigniter4/framework": "^' . $version . '"',
+    $composerJson
+);
+file_put_contents($composerJsonPath, $newComposerJson);
