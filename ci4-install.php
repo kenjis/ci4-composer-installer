937376ed75094fb6a9eb483a735ce68788e1c68d
kenjis <kenji.uui@gmail.com>
2022-07-18 13:04:33 +0900

    refactor: use proc_open() instead of passthru()

--- ci4-install.php
+++ ci4-install.php
@@ -13,26 +13,59 @@ if ($argc < 2) {
 
 $dir = $argv[1];
 
+function runCommand(array $command): void
+{
+    echo '[ci4-install] ' . implode(' ', $command) . PHP_EOL;
+
+    $descriptor = [
+        0 => STDIN,
+        1 => STDOUT,
+        2 => STDERR,
+    ];
+    $process = proc_open($command, $descriptor, $pipes);
+
+    if (! is_resource($process)) {
+        throw new RuntimeException(
+            'Cannot execute command: "' . implode(' ', $command). '"'
+        );
+    }
+
+    $status = proc_get_status($process);
+    while ($status['running']) {
+        usleep(100000);
+        $status = proc_get_status($process);
+    }
+
+    proc_close($process);
+
+    $exitCode = $status['exitcode'];
+    //echo $exitCode . PHP_EOL;
+
+    if ($exitCode !== 0) {
+        throw new RuntimeException(
+            'Command failed: "' . implode(' ', $command) . '"'
+        );
+    }
+}
+
 // install the latest version
 if ($argc === 2) {
-    $command = 'composer create-project codeigniter4/appstarter ' . $dir;
-    echo '[ci4-install] ' . $command . PHP_EOL;
-    passthru($command);
+    $command = ['composer', 'create-project', 'codeigniter4/appstarter', $dir];
+    runCommand($command);
 
     exit(0);
 }
 
+// install the specific version
 $version = $argv[2];
 
-$command = 'composer create-project codeigniter4/appstarter:' . $version . ' ' . $dir;
-echo '[ci4-install] ' . $command . PHP_EOL;
-passthru($command);
+$command = ['composer', 'create-project', 'codeigniter4/appstarter:'.$version, $dir];
+runCommand($command);
 
 chdir($dir);
 
-$command = 'composer require -W codeigniter4/framework:' . $version;
-echo '[ci4-install] ' . $command . PHP_EOL;
-passthru($command);
+$command = ['composer', 'require', '-W', 'codeigniter4/framework:'. $version];
+runCommand($command);
 
 echo '[ci4-install] ' . 'update "codeigniter4/framework" version in composer.json'. PHP_EOL;
 $composerJsonPath = "composer.json";
