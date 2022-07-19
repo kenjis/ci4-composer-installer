15fba094dfb2b56fea61c3141e5a766c69a7906c
kenjis <kenji.uui@gmail.com>
2022-07-19 09:38:44 +0900

    refactor: move functions up

--- ci4-install.php
+++ ci4-install.php
@@ -1,17 +1,9 @@
 #!/usr/bin/env php
 <?php
 
-if ($argc < 2) {
-    file_put_contents(
-        'php://stderr',
-        'Usage: php ci4-install.php <directory> [<version>]' . PHP_EOL
-        . '   Eg: php ci4-install.php ci4app 4.1.9' . PHP_EOL
-    );
-
-    exit(1);
-}
-
-$dir = $argv[1];
+//--------------------------------------------------------------------
+// Functions
+//--------------------------------------------------------------------
 
 function runCommand(array $command): void
 {
@@ -48,6 +40,35 @@ function runCommand(array $command): void
     }
 }
 
+function updateFrameworkVersion(string $version): void
+{
+    echo '[ci4-install] ' . 'update "codeigniter4/framework" version in composer.json' . PHP_EOL;
+    $composerJsonPath = "composer.json";
+    $composerJson = file_get_contents($composerJsonPath);
+    $newComposerJson = preg_replace(
+        '!"codeigniter4/framework": ".*?"!',
+        '"codeigniter4/framework": "^' . $version . '"',
+        $composerJson
+    );
+    file_put_contents($composerJsonPath, $newComposerJson);
+}
+
+//--------------------------------------------------------------------
+// Main
+//--------------------------------------------------------------------
+
+if ($argc < 2) {
+    file_put_contents(
+        'php://stderr',
+        'Usage: php ci4-install.php <directory> [<version>]' . PHP_EOL
+        . '   Eg: php ci4-install.php ci4app 4.1.9' . PHP_EOL
+    );
+
+    exit(1);
+}
+
+$dir = $argv[1];
+
 // install the latest version
 if ($argc === 2) {
     $command = ['composer', 'create-project', 'codeigniter4/appstarter', $dir];
@@ -68,16 +89,3 @@ $command = ['composer', 'require', '-W', 'codeigniter4/framework:' . $version];
 runCommand($command);
 
 updateFrameworkVersion($version);
-
-function updateFrameworkVersion(string $version): void
-{
-    echo '[ci4-install] ' . 'update "codeigniter4/framework" version in composer.json' . PHP_EOL;
-    $composerJsonPath = "composer.json";
-    $composerJson = file_get_contents($composerJsonPath);
-    $newComposerJson = preg_replace(
-        '!"codeigniter4/framework": ".*?"!',
-        '"codeigniter4/framework": "^' . $version . '"',
-        $composerJson
-    );
-    file_put_contents($composerJsonPath, $newComposerJson);
-}
